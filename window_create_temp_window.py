5383e676b9ebd796172a7485be394ab3ba928d0a
TheKenetics <TheKenetics@users.noreply.github.com>
2019-03-21 15:49:42 -0500

    Add files via upload

---
+++ window_create_temp_window.py
@@ -0,0 +1,96 @@
+bl_info = {
+	"name": "Create Temp Window",
+	"author": "Kenetics",
+	"version": (0, 1),
+	"blender": (2, 80, 0),
+	"location": "View3D > Right Click Context Menu",
+	"description": "Creates a new window from 3D View",
+	"warning": "",
+	"wiki_url": "",
+	"category": "System"
+}
+
+import bpy
+from bpy.props import EnumProperty, IntProperty, FloatVectorProperty, BoolProperty, FloatProperty, StringProperty
+
+def create_temp_window(context, window_type):
+	# Call user prefs window
+	bpy.ops.screen.userpref_show('INVOKE_DEFAULT')
+	
+	if window_type == "ADDONS":
+		context.preferences.active_section = "ADDONS"
+		
+	elif window_type != "USER_PREFERENCES":
+		# Change area type
+		area = context.window_manager.windows[-1].screen.areas[0]
+		area.type = window_type
+
+
+class CTW_OT_create_temp_window(bpy.types.Operator):
+	"""Creates a new window"""
+	bl_idname = "object.ctw_ot_create_temp_window"
+	bl_label = "Create Temp Window"
+	bl_options = {'REGISTER','UNDO'}
+	
+	# Properties
+	window_type : EnumProperty(
+		items=[
+			("VIEW_3D","3D View","","VIEW3D",0),
+			("GRAPH_EDITOR","Graph Editor","","GRAPH",1),
+			("DOPESHEET_EDITOR","Dopesheet","","GRAPH",2),
+			("NLA_EDITOR","NLA Editor","","NLA",3),
+			("IMAGE_EDITOR","Image Editor","","FILE_IMAGE",4),
+			("TEXT_EDITOR","Text Editor","","FILE_TEXT",5),
+			("NODE_EDITOR","Node Editor","","NODE_MATERIAL",6),
+			("PROPERTIES","Properties","","PROPERTIES",7),
+			("OUTLINER","Outliner","","OUTLINER",8),
+			("USER_PREFERENCES","Preferences","","PREFERENCES",9),
+			("FILE_BROWSER","File Browser","","FILE_FOLDER",10),
+			("CONSOLE","Console","","CONSOLE",11),
+			("ADDONS","Addons","","PREFERENCES",12)
+			],
+		name="Window Type",
+		description="Type of window to create",
+		default="USER_PREFERENCES"
+	)
+
+	@classmethod
+	def poll(cls, context):
+		return True
+
+	def execute(self, context):
+		create_temp_window(context, self.window_type)
+		return {'FINISHED'}
+
+
+def create_temp_window_button(self, context):
+	self.layout.operator_menu_enum(CTW_OT_create_temp_window.bl_idname, "window_type", icon="WINDOW")
+
+def register():
+	bpy.utils.register_class(CTW_OT_create_temp_window)
+	bpy.types.VIEW3D_MT_object_context_menu.append(create_temp_window_button)
+
+def unregister():
+	bpy.types.VIEW3D_MT_object_context_menu.remove(create_temp_window_button)
+	bpy.utils.unregister_class(CTW_OT_create_temp_window)
+
+if __name__ == "__main__":
+	register()
+	
+"""
+GRAPH_EDITOR Graph Editor, Edit drivers and keyframe interpolation.
+DOPESHEET_EDITOR Dope Sheet, Adjust timing of keyframes.
+NLA_EDITOR NLA Editor, Combine and layer Actions.
+IMAGE_EDITOR UV/Image Editor, View and edit images and UV Maps.
+SEQUENCE_EDITOR Video Sequence Editor, Video editing tools.
+CLIP_EDITOR Movie Clip Editor, Motion tracking tools.
+TEXT_EDITOR Text Editor, Edit scripts and in-file documentation.
+NODE_EDITOR Node Editor, Editor for node-based shading and compositing tools.
+LOGIC_EDITOR Logic Editor, Game logic editing.
+PROPERTIES Properties, Edit properties of active object and related datablocks.
+OUTLINER Outliner, Overview of scene graph and all available datablocks.
+USER_PREFERENCES User Preferences, Edit persistent configuration settings.
+INFO Info, Main menu bar and list of error messages (drag down to expand and display).
+FILE_BROWSER File Browser, Browse for files and assets.
+CONSOLE
+"""
